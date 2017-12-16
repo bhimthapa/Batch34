830e2a35d9f0353997c33aec37816e64bc566855
bhimthapa <thapa.bhim999@gmail.com>
2017-12-15 21:39:31 -0500

    Added new batch34 Project

---
+++ SM/Login.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SM.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblErrorMessage" BackColor="Red" runat="server" Text=""></asp:Label>
+            <table>
+                <tr>
+                    <td>User Name</td>
+                    <td>
+                        <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>Password</td>
+                    <td>
+                        <asp:TextBox ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+                    </td>
+                </tr>
+            </table>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SM/Login.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SM
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Connect to Db server and database
+            SqlConnection con = new SqlConnection("Data Source=WINDOWS-5MOB6K6\\SQLEXPRESS;Initial Catalog=SMDB;Integrated Security=True");
+            //Set query
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from UserLogin", con);
+            //Read data from table
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            while(rd.Read())
+            {
+                if (txtUserName.Text == rd.GetString(1).ToString() && txtPassword.Text == rd.GetString(2).ToString())
+                {
+                    Response.Redirect("Home.aspx");
+                }
+            }
+
+           
+   
+                lblErrorMessage.Text = "Access denied";
+            
+        }
+    }
+}
